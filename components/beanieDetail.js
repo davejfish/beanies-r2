36b534b5034bb4c6b379a4ddd2488c5be0405511
Dave Fisher <dave.j.fish@gmail.com>
2022-06-22 17:55:12 -0700

    added all data parameters

--- components/beanieDetail.js
+++ components/beanieDetail.js
@@ -2,7 +2,7 @@ export default function createBeanieDetail(root) {
     const title = document.querySelector('#name');
     const img = document.querySelector('#bb-image');
     const BPLink = document.querySelector('#link');
-    const [theme, birthday, releaseDate, starSign] = root.querySelectorAll('p');
+    const [theme, birthday, releaseDate, releaseYear, starSign, styleNum, color, STG, TTG, retirementDate, animal] = root.querySelectorAll('p');
 
     return ({ beanie }) => {
         title.textContent = beanie.title;
@@ -10,13 +10,19 @@ export default function createBeanieDetail(root) {
         theme.textContent = `theme: ${beanie.theme}`;
         birthday.textContent = `birthday: ${beanie.birthday}`;
         releaseDate.textContent = `released on: ${beanie.releaseDate}`;
+        releaseYear.textContent = `release year: ${beanie.releaseYear}`;
         
         if (beanie.astroSign) {
             starSign.textContent = `star sign: ${beanie.astroSign}`;
         }
-        else {
-            starSign.innerHTML = '';
-        }
+
+        styleNum.textContent = `style number: ${beanie.styleNumber}`;
+        color.textContent = `color: ${beanie.color}`;
+        STG.textContent = `swing tag gen: ${beanie.swingTagGeneration}`;
+        TTG.textContent = `tush tag: ${beanie.tushTagGeneration}`;
+        retirementDate.textContent = `retired on: ${beanie.retirementDate}`;
+        animal.textContent = `animal: ${beanie.animal}`;
+
         BPLink.href = beanie.link;
     };
 }
